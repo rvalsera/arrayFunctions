3f92dc9b2347744b9c2d1f9d360d8879c66907b4
rvalsera <raul.valsera@king.com>
2019-05-27 15:42:01 +0200

    Update Array.forEach.js

--- Array.forEach.js
+++ Array.forEach.js
@@ -12,15 +12,13 @@ array1.forEach(function(element, i) {
 });
 
 function forEach(func, thing) {
-    let a = func;
-    let b = thing;
-    if (Object.prototype.toString.call(a) !== "[object Function]" || Object.prototype.toString.call(b) !== "[object Array]") {
+    if (Object.prototype.toString.call(func) !== "[object Function]" || Object.prototype.toString.call(thing) !== "[object Array]") {
         console.log("The parameters provided are incorrect.\nYou must provide a Function and an Array.");
-        console.log("\nParameter 1: " + Object.prototype.toString.call(a));
-        console.log("\nParameter 2: " + Object.prototype.toString.call(b));
+        console.log("\nParameter 1: " + Object.prototype.toString.call(func));
+        console.log("\nParameter 2: " + Object.prototype.toString.call(thing));
         } else {
-            for (let i = 0; i < b.length; i++) {
-                doSomething(b[i]);
+            for (let i = 0; i < thing.length; i++) {
+                func(b[i]);
             };
     }
 };
